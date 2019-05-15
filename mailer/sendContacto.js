9e9f8bc02f3a571e19f0db25c23b81b667ad4675
Pablo Pizarro <pabloquero.misc@gmail.com>
2019-05-15 12:12:26 -0400

    mail transp

--- mailer/sendContacto.js
+++ mailer/sendContacto.js
@@ -16,10 +16,9 @@ module.exports = function sendContacto({
 
   let info = transporter.sendMail({
     from: '"Inmobiliaria Fuenzalida" <no-reply@inmobiliariafuenzalida.com>',
-    to: "ppizarro@grupobyl.cl",
+    to:
+      "info@inmobiliariafuenzalida.com, alejandraopazo@inmobiliariafuenzalida.com",
     subject: `Nuevo mensaje desde formulario ${categoria}`,
-    html: `Nuevo envío de correo desde compra de propiedades:<br/>Nombre: ${nombre}<br/>Teléfono:${telefono}<br/>Mail: ${mail}<br/>Mensaje: ${mensaje}<br/>Categoria: ${categoria}<br/>` // html body
+    html: `Nuevo envío de correo desde formulario ${categoria}:<br/>Nombre: ${nombre}<br/>Teléfono:${telefono}<br/>Mail: ${mail}<br/>Mensaje: ${mensaje}<br/>Categoria: ${categoria}<br/>` // html body
   });
-
-  console.log("Message sent: %s", info.messageId);
 };
