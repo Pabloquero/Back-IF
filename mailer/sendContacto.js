79b06108bfcf09fc3a458bf0d1abee0f5c5b388d
Pablo Pizarro <pabloquero.misc@gmail.com>
2019-05-15 12:03:35 -0400

    mail transp

--- mailer/sendContacto.js
+++ mailer/sendContacto.js
@@ -1,27 +1,23 @@
 "use strict";
 const nodemailer = require("nodemailer");
 
-// async..await is not allowed in global scope, must use a wrapper
 module.exports = function sendContacto({
   args: { nombre, telefono, mail, mensaje, categoria }
 }) {
-  // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
     secure: true, // true for 465, false for other ports
     auth: {
-      user: "no-reply@inmobiliariafuenzalida.com", // generated ethereal user
-      pass: "noreply08012018" // generated ethereal password
+      user: "no-reply@inmobiliariafuenzalida.com",
+      pass: "noreply08012018"
     }
   });
 
-  // send mail with defined transport object
   let info = transporter.sendMail({
-    from: '"Prueba 2 👻" <no-reply@inmobiliariafuenzalida.com>', // sender address
-    to: "ppizarro@grupobyl.cl", // list of receivers
-    subject: "Prueba fuenzalida 2 ✔", // Subject line
-    text: "TEST 2", // plain text body
+    from: '"Inmobiliaria Fuenzalida" <no-reply@inmobiliariafuenzalida.com>',
+    to: "ppizarro@grupobyl.cl",
+    subject: `Nuevo mensaje desde formulario ${categoria}`,
     html: `Nuevo envío de correo desde compra de propiedades:<br/>Nombre: ${nombre}<br/>Teléfono:${telefono}<br/>Mail: ${mail}<br/>Mensaje: ${mensaje}<br/>Categoria: ${categoria}<br/>` // html body
   });
 
